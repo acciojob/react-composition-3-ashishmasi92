1005aead5286722754053321fe30b3dee4aaefd7
ashish <ashishmasih92g@gmail.com>
2025-06-16 10:22:51 +0530

    initialize a assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,21 @@ import './../styles/App.css';
 import Tooltip from "./Tooltip";
 const App = () => {
   return (
-    <div style={{display:"inline-block", padding:"100px", position:"relative"}}>
+    <div  style={{ display: "inline-block", padding: "100px", position: "relative" }}>
 
-<Tooltip
-text={"This is a Tooltip"}
->
-   <h2 style={{border:"none", backgroundColor:"transparent", borderBottom:"1px solid black"}} className="tooltip" >Hover over me</h2>
-</Tooltip>
+      <Tooltip
+        text={"This is a Tooltip"}
+      >
+        <h2 data-testid="tooltip-target-1" style={{ border: "none", backgroundColor: "transparent", borderBottom: "1px solid black" }} >Hover over me</h2>
+      </Tooltip>
 
 
 
-<Tooltip
-text={"this is another Tooltip"}
->
-<button style={{border:"none", backgroundColor:"transparent", borderBottom:"1px solid black", marginTop:"150px" }} ><p>Hover over me to see another Tooltip</p></button>
-</Tooltip>
+      <Tooltip
+        text={"this is another Tooltip"}
+      >
+        <button data-testid="tooltip-target-2" style={{ border: "none", backgroundColor: "transparent", borderBottom: "1px solid black", marginTop: "150px" }} ><p>Hover over me to see another Tooltip</p></button>
+      </Tooltip>
 
     </div>
   )
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,27 +2,32 @@ import React, { useState } from "react"
 
 
 
-const Tooltip = ({text,children})=>{
-const [show ,setShow] = useState(false)
+const Tooltip = ({ text, children }) => {
+    const [show, setShow] = useState(false)
 
 
-const show1 = ()=>{
-    setShow(true)
-}
-const hide = ()=>{
-    setShow(false)
-}
+    const show1 = () => {
+        setShow(true)
+    }
+    const hide = () => {
+        setShow(false)
+    }
 
 
     return (
-       
-        <div 
-        onMouseEnter={show1}
-        onMouseLeave={hide}
+
+        <div
+            onMouseEnter={show1}
+            onMouseLeave={hide}
+            style={{ display: "inline-block", position: "relative" }}
         >
-            {show&& <div  className="tooltip" style={{ position: "absolute", border:"none", textAlign:"center", width:"100px", backgroundColor:"red", margin:"20px ", padding:"10px", borderRadius:"10px", borderInlineStyle:"none"}}><p>{text}</p></div>}
-         {children}
-            
+            {show && <div
+                className="tooltip"
+                data-testid="tooltip"
+
+                style={{ position: "absolute", border: "none", textAlign: "center", width: "100px", backgroundColor: "red", margin: "20px ", padding: "10px", borderRadius: "10px", borderInlineStyle: "none" }}><p>{text}</p></div>}
+            {children}
+
         </div>
     )
 }
